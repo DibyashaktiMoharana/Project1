2ae31132cf279dd6838dfba35503ccdea2a8d802
Dibyashakti <dibyacollege@gmail.com>
2023-05-21 21:04:48 +0530

    second commit

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const index = __dirname+ "/index.html"
 
+app.set("view engine","ejs")
+
+app.use(express.static("public"))
 app.get("/", function(req,res){
-    res.sendFile(index);
+    // res.sendFile(index);
+    res.render("index");
 });
+
 
 app.listen(3000,function(){
     console.log("Server started at port 3000");

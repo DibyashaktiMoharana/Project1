65eb16bfe6269c529adaa614b2348cae2e45b1c6
Dibyashakti <dibyacollege@gmail.com>
2023-05-24 11:54:32 +0530

    Fixed the toggle switch click area,visual chanegs

--- public/navbar.js
+++ public/navbar.js
@@ -3,12 +3,14 @@ var element = document.getElementById("navbart");
 // document.getElementsByClassName("logot").style.transition - "all 2s"
 function myfunc() {
     if(element.style.border === '0px'){
-        element.style.width = '15%';
+        element.style.width = '20%';
         element.style.border = 'none';
         document.getElementById("w").style.color= 'aliceblue';
+        document.getElementById("togglecontainer").style.backgroundColor = 'blue';
     }else{
         element.style.width ='0%';
         element.style.border = '0px';
         document.getElementById("w").style.color= 'black';
+        document.getElementById("togglecontainer").style.backgroundColor='';
     }
 }
